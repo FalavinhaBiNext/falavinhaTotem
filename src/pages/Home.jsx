6a0dce33559741ac400f34f2344d9b0040218983
giovane_falavinha <giovane.pereira@falavinha.com.br>
2024-10-24 08:40:39 -0300

    add: video com audio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,31 @@ import HeaderApp from "../components/Header";
 import HeroApp from "../components/Hero";
 import FooterApp from "../components/Footer";
 import fundo from "../assets/video/video-demo.mp4";
+import { useState } from "react";
 
 function HomePage() {
+  const [isMuted, setIsMuted] = useState(true);
+  const audioOverlay = {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: "100",
+    opacity: "0",
+  };
+
   return (
     <>
       <HeaderApp />
       <HeroApp>
-        <video autoPlay muted loop className="home-video">
-          <source className="home-video__video" src={fundo} type="video/mp4" />
+        <video autoPlay loop muted={isMuted} playsInline className="home-video">
+          <source src={fundo} type="video/mp4" />
         </video>
+        <span style={audioOverlay} onClick={() => setIsMuted(!isMuted)}></span>
       </HeroApp>
 
       <FooterApp />
@@ -19,5 +35,3 @@ function HomePage() {
 }
 
 export default HomePage;
-
-// home-video
